2bd2e530adedf65d385836a38357f162c3ba9e6e
tantv-918 <SUN-ASTERISK\trinh.van.tan@b120918-pc.sun-asterisk.com>
2019-10-01 11:29:02 +0700

    Route Teacher

--- web-app/server/routes/teacher.js
+++ web-app/server/routes/teacher.js
@@ -1,21 +1,103 @@
 const router = require('express').Router();
-const controller = require('../controllers/teacher');
+const USER_ROLES = require('../configs/constant').USER_ROLES;
+const networkAcademy = require('../fabric/networkAcademy');
+const User = require('../models/User');
 
 router.get('/create', async (req, res) => {
-    res.json({
-        hello: 'new teacher'
-    });
+    if (req.decoded.user.role !== USER_ROLES.ADMIN_ACADEMY) {
+        res.json({
+            success: false,
+            msg: 'Failed'
+        });
+    } else {
+        res.json({
+            hello: 'new teacher'
+        });
+    }
 });
 
 router.post('/create',
     [
-        check('username').isLength({min: 6})
+        check('username').isLength({min: 6}),
+        check('name').isLength({min: 5}),
     ],
-    controller.postCreate
+    async (req, res, next) => {
+        if (req.decoded.user.role !== USER_ROLES.ADMIN_ACADEMY) {
+            res.json({
+                success: false,
+                msg: 'Failed'
+            });
+        } else {
+            let teacher = new User({
+                name: req.body.name,
+                username: req.body.username,
+                password: '123456',
+                role: USER_ROLES.TEACHER
+            });
+
+            User.findOne({username: teacher.username}, async (err, existing) => {
+                if (err) throw next(err);
+                if (existing) {
+                    res.json({
+                        success: false,
+                        msg: 'Account is exist'
+                    });
+                } else {
+                    await teacher.save();
+                    await networkAcademy.registerTeacher(teacher.username);
+                }
+            })
+
+        }
+    }
 );
 
-router.get('/', controller.getListTeacher);
+router.get('/', async (req, res, next) => {
+    if (req.decoded.user.role !== USER_ROLES.ADMIN_ACADEMY) {
+        res.json({
+            success: false,
+            msg: 'Failed'
+        });
+    } else {
+        User.find({role: USER_ROLES.TEACHER}, async (err, teachers) => {
+            if (err) {
+                res.json({
+                    success: false,
+                    msg: err
+                })
+            } else {
+                res.json({
+                    success: true,
+                    teachers: teachers
+                })
+            }
+        });
+    }
+});
 
-router.get('/:username', controller.get);
+router.get('/:username', async (req, res, next) => {
+    if (req.decoded.user.role !== USER_ROLES.ADMIN_ACADEMY) {
+        res.json({
+            success: false,
+            msg: 'Failed'
+        });
+    } else {
+        var username = req.params.username;
+
+        User.findOne({username: username}, async (err, teacher) => {
+            if (err) {
+                res.json({
+                    success: false,
+                    msg: err
+                });
+            } else {
+                res.json({
+                    success: true,
+                    teacher: teacher
+                });
+            }
+        });
+    }
+});
 
 module.exports = router;
